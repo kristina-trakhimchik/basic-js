c5680900bbf420f4aa4272a49adf4cc5921005fd
kristina-trakhimchik <dilfinium_kristi@mail.ru>
2023-12-26 01:43:01 +0300

    feat: solve hanoi-towel

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed) {
+  let turns1 = Math.pow(2, disksNumber) - 1;
+  const tim = Math.trunc(turns1 / turnsSpeed * 60 * 60);
+  let data = {
+    turns: turns1,
+    seconds: tim,
+  }
+  return data;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+
+  const datePars = Date.parse(date);
+  if (isNaN(datePars) || !date instanceof Date || date.toString() == "[object type]") {
+    return 'Invalid date!';
+  }
+    const month = new Date(datePars).getMonth() + 1;
+    switch (month) {
+      case 1 :
+      case 2 :
+      case 12 :
+        return 'winter';
+      case 3 :
+      case 4 :
+      case 5 :
+        return 'spring';
+      case 6 :
+      case 7 :
+      case 8 :
+        return 'summer';
+      case 9 :
+      case 10 :
+      case 11 :
+        return 'autumn';
+    }
+  }
 
 module.exports = {
   getSeason
